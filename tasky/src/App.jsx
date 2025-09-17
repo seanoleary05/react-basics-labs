f1a65d3c6c0c794809ef534006d34273e2bec93b
seanoleary05 <20106716@mail.wit.ie>
2025-09-17 22:13:26 +0100

    added css

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -3,11 +3,12 @@ import Task from './components/Task';
 
 function App(){
   return (
-    <div className='App'>
+    <div className='container'>
       <h1>Tasky</h1>
       <Task title="Dishes" deadline="Today"/>
-      <Task title="Laundry" deadline="Tomorrow"/>
+      <Task title="Laundry" deadline="Tomorrow">
         Fold laundry and put away
+        </Task>
       <Task title="Tidy" deadline="Today"/>
 
     </div>
--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
+
 const Task = (props) => {
 
         return (
-            <div>
-            <p>{props.title}</p>
+            <div className ="card">
+            <p className ="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p>{props.children}</p>
             </div>
